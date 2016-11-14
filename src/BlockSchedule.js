e856da782be348522d61f90761d44574f61681b3
Karina Antonio <karinafantonio@gmail.com>
2016-11-13 22:53:26 -0800

    Create grid aligning CourseBlocks with their times

--- src/BlockSchedule.js
+++ src/BlockSchedule.js
@@ -1,15 +1,96 @@
 import React from 'react'
 import CourseBlock from './CourseBlock'
+import './BlockSchedule.css'
 
-const BlockSchedule = ({courses}) => (
-  <div>
-    {courses.map(course =>
-      <CourseBlock
-        course={course}
-        key={course.crn}
-      />
-    )}
-  </div>
-)
+function timesOverlap(time1, time2) {
+  return time1.start <= time2.end && time2.start <= time1.end
+}
+
+function minuteCssOffset(minute) {
+  return (minute * 0.075) + "em"
+}
+
+const BlockSchedule = ({courses}) => {
+  // Find the earliest and latest hours to render
+  let earliestHour = 24;
+  let latestHour = 0;
+  for (let course of courses) {
+    let currentStartHour = course.start.hour
+    let currentEndHour = course.end.hour
+    if (currentStartHour < earliestHour) {
+      earliestHour = currentStartHour
+    }
+    if (currentEndHour >= latestHour) {
+      latestHour = currentEndHour + 1
+    }
+  }
+
+  // First, separate courses by day and by hour in each day
+  let weekdays = { M: [], T: [], W: [], R: [], F: [] }
+  for (let course of courses) {
+    let i = course.days.length
+    while (i--) {
+      if (typeof weekdays[course.days[i]][course.start.hour] === "undefined") {
+        weekdays[course.days[i]][course.start.hour] = [course]
+      }
+      else {
+        weekdays[course.days[i]][course.start.hour].push(course)
+      }
+    }
+  }
+  // Finally, create elements based on this structure (basically a 3D array)
+  // And be sure to check for and deal with overlapping courses at this stage!
+  let weekdayElements = { M: [], T: [], W: [], R: [], F: [] }
+  for (let day in weekdays) {
+    for (let hour = earliestHour; hour <= latestHour; ++hour) {
+      if (typeof weekdays[day][hour] === "undefined") {
+        weekdayElements[day][hour] = <div className="hour" key={day + hour}></div>
+      }
+      else {
+        let courseElements = []
+        for (let course of weekdays[day][hour]) {
+          // TODO: Check all coursesPerDay for overlap and apply special CSS if so
+          courseElements.push(
+            <CourseBlock
+              course={course}
+              key={course.crn}
+              style={{marginTop: minuteCssOffset(course.start.minute)}}
+            />
+          )
+        }
+        weekdayElements[day][hour] = (
+          <div className="hour" key={day + hour}>
+            {[...courseElements]}
+          </div>
+        )
+      }
+    }
+  }
+
+  return (
+    <div className="BlockSchedule">
+      <div className="weekday">
+        <p className="weekdayLabel">M</p>
+        {weekdayElements.M}
+      </div>
+      <div className="weekday">
+        <p className="weekdayLabel">T</p>
+        {weekdayElements.T}
+      </div>
+      <div className="weekday">
+        <p className="weekdayLabel">W</p>
+        {weekdayElements.W}
+      </div>
+      <div className="weekday">
+        <p className="weekdayLabel">R</p>
+        {weekdayElements.R}
+      </div>
+      <div className="weekday">
+        <p className="weekdayLabel">F</p>
+        {weekdayElements.F}
+      </div>
+    </div>
+  )
+}
 
 export default BlockSchedule
--- src/CourseBlock.js
+++ src/CourseBlock.js
@@ -2,11 +2,12 @@ import React from 'react'
 import {to12HourStringNoMeridiem as timeToStringNoMeridiem, militaryTime} from './util/Time'
 import './CourseBlock.css'
 
-const CourseBlock = ({course}) => {
+const CourseBlock = ({course, style}) => {
   let start = course.start
   let end = course.end
+  let height = ((militaryTime(end) - militaryTime(start)) / 25.5) + "em"
   return (
-    <div className="CourseBlock">
+    <div className="CourseBlock" style={{...style, height}}>
       <p className="name">{course.name}</p>
       <p className="time">
         {timeToStringNoMeridiem(start)} - {timeToStringNoMeridiem(end)}
