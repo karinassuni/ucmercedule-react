94d9d913a07a2cb137c3276e8cfeec74ecd5de73
Karina Antonio <karinafantonio@gmail.com>
2016-11-13 22:08:43 -0800

    Extract Weekday React component

--- src/BlockSchedule.js
+++ src/BlockSchedule.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import HourLabels from './HourLabels'
-import CourseBlock from './CourseBlock'
+import Weekday from './Weekday'
 import './BlockSchedule.css'
 
-function timesOverlap(time1, time2) {
-  return time1.start <= time2.end && time2.start <= time1.end
-}
-
-function minuteCssOffset(minute) {
-  return (minute * 0.075) + "em"
-}
-
-const BlockSchedule = ({courses}) => {
-  // Find the earliest and latest hours to render
+function getEarliestAndLatestHours(courses) {
   let earliestHour = 24;
   let latestHour = 0;
   for (let course of courses) {
@@ -25,45 +16,22 @@ const BlockSchedule = ({courses}) => {
       latestHour = currentEndHour + 1
     }
   }
+  return {earliestHour, latestHour}
+}
 
-  // First, separate courses by day and by hour in each day
+const BlockSchedule = ({courses}) => {
+  let {earliestHour, latestHour} = getEarliestAndLatestHours(courses)
+
+  // Separate courses by day and by hour in each day
   let weekdays = { M: [], T: [], W: [], R: [], F: [] }
   for (let course of courses) {
     let i = course.days.length
     while (i--) {
-      if (typeof weekdays[course.days[i]][course.start.hour] === "undefined") {
+      if (weekdays[course.days[i]][course.start.hour] === undefined) {
         weekdays[course.days[i]][course.start.hour] = [course]
       }
       else {
         weekdays[course.days[i]][course.start.hour].push(course)
-      }
-    }
-  }
-  // Finally, create elements based on this structure (basically a 3D array)
-  // And be sure to check for and deal with overlapping courses at this stage!
-  let weekdayElements = { M: [], T: [], W: [], R: [], F: [] }
-  for (let day in weekdays) {
-    for (let hour = earliestHour; hour <= latestHour; ++hour) {
-      if (typeof weekdays[day][hour] === "undefined") {
-        weekdayElements[day][hour] = <div className="hour" key={day + hour}></div>
-      }
-      else {
-        let courseElements = []
-        for (let course of weekdays[day][hour]) {
-          // TODO: Check all coursesPerDay for overlap and apply special CSS if so
-          courseElements.push(
-            <CourseBlock
-              course={course}
-              key={course.crn}
-              style={{marginTop: minuteCssOffset(course.start.minute)}}
-            />
-          )
-        }
-        weekdayElements[day][hour] = (
-          <div className="hour" key={day + hour}>
-            {[...courseElements]}
-          </div>
-        )
       }
     }
   }
@@ -72,28 +40,17 @@ const BlockSchedule = ({courses}) => {
     <div className="BlockSchedule">
       <HourLabels
         earliestHour={earliestHour}
-        latestHour={latestHour}
-      ></HourLabels>
-      <div className="weekday">
-        <p className="weekdayLabel">M</p>
-        {weekdayElements.M}
-      </div>
-      <div className="weekday">
-        <p className="weekdayLabel">T</p>
-        {weekdayElements.T}
-      </div>
-      <div className="weekday">
-        <p className="weekdayLabel">W</p>
-        {weekdayElements.W}
-      </div>
-      <div className="weekday">
-        <p className="weekdayLabel">R</p>
-        {weekdayElements.R}
-      </div>
-      <div className="weekday">
-        <p className="weekdayLabel">F</p>
-        {weekdayElements.F}
-      </div>
+        latestHour={latestHour}>
+      </HourLabels>
+      {Object.keys(weekdays).map((day) => (
+        <Weekday
+          day={day}
+          key={day}
+          coursesPerHour={weekdays[day]}
+          earliestHour={earliestHour}
+          latestHour={latestHour}
+        ></Weekday>
+      ))}
     </div>
   )
 }
---
+++ src/Weekday.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import CourseBlock from './CourseBlock'
+import './Weekday.css'
+
+function minuteCssOffset(minute) {
+  return (minute * 0.075) + "em"
+}
+
+function timesOverlap(time1, time2) {
+  return time1.start <= time2.end && time2.start <= time1.end
+}
+
+const Weekday = ({day, coursesPerHour, earliestHour, latestHour}) => {
+  let hourElements = []
+  for (let i = earliestHour; i <= latestHour; ++i) {
+    if (coursesPerHour[i] === undefined) {
+      // Create an empty hour element
+      hourElements[i] = <div className="hour" key={i}></div>
+    }
+    else {
+      // Create an hour element filled with courses during that hour
+      let courseElements = []
+      for (let course of coursesPerHour[i]) {
+        // TODO: Check all coursesPerDay for overlap and apply special CSS if so
+        courseElements.push(
+          <CourseBlock
+          course={course}
+          key={course.crn}
+          style={{marginTop: minuteCssOffset(course.start.minute)}}
+          />
+        )
+      }
+      hourElements[i] = (
+        <div className="hour" key={i}>
+        {[...courseElements]}
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div className="Weekday">
+      <p className="weekdayLabel">{day}</p>
+      {[...hourElements]}
+    </div>
+  )
+}
+
+export default Weekday
