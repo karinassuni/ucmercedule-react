dc446459d8cfe457f418137c7a3a556dfb2988ec
Karina Antonio <karinafantonio@gmail.com>
2016-11-13 22:48:53 -0800

    Correct test data

--- src/CourseBlock.test.js
+++ src/CourseBlock.test.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import CourseBlock from './CourseBlock';
 
 const course = {
-    name: "Electrodynamics Core II Waves",
-    cnum: "PHYS-115-01",
+    name: "Anthropological Archaeology",
+    cnum: "ANTH-003-01",
     type: "LECT",
-    crn: 15390,
+    crn: 10265,
     start: {hour: 11, minute: 30},
     end: {hour: 13, minute: 15},
     days: "TR"
